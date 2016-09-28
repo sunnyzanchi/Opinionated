1991ebb7d8aca3a36da2e115ed72ab3b3501cdaf
Eric Zanchi <emzanchi@gmail.com>
2016-09-28 10:24:12 -0500

    Added favicon

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+const favicon = require('serve-favicon');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -19,6 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'vash');
 /* Public */
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 /* Routes */
 //Index
