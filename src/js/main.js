cde960dca7da157a45fd8f7278f0aa3d1d94057e
Eric Zanchi <emzanchi@gmail.com>
2016-12-02 15:50:34 -0600

    Added icon font to fontForceLoad

--- src/js/main.js
+++ src/js/main.js
@@ -87,8 +87,15 @@ require('./initws.js')(app);
 // Otherwise, it would wait to download it until some DOM with Roboto font was rendered
 // This can cause it to jitter or even not show the text under slow network conditions
 (function fontForceLoad(){
-  var font = new FontFace('Roboto', 'url(https://fonts.gstatic.com/s/roboto/v15/Hgo13k-tfSpn0qi1SFdUfVtXRa8TVwTICgirnJhmVJw.woff2)', {});
-  font.load().then(function(){
-    document.fonts.add(font);
+  var roboto = new FontFace('Roboto', 'url(https://fonts.gstatic.com/s/roboto/v15/Hgo13k-tfSpn0qi1SFdUfVtXRa8TVwTICgirnJhmVJw.woff2)', {});
+  var icons = new FontFace('Material Icons', 'url(https://fonts.gstatic.com/s/materialicons/v19/2fcrYFNaTjcS6g4U3t-Y5ZjZjT5FdEJ140U2DJYC3mY.woff2)', {});
+
+  roboto.load().then(function(){
+    document.fonts.add(roboto);
   });
+
+  icons.load().then(function(){
+    documents.fonts.add(icons);
+  });
+
 }());
