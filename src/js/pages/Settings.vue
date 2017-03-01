ce8dbcd4ab8ec72538b2ff928efd8043c2d72b50
Eric Zanchi <emzanchi@gmail.com>
2017-03-01 11:11:57 -0600

    Added settings page

---
+++ src/js/pages/Settings.vue
@@ -0,0 +1,15 @@
+<style lang="css">
+  .settings-container{
+    height: 100%;
+  }
+</style>
+<!-- ### -->
+<template lang="html">
+  <div class="settings-container">
+  </div>
+</template>
+<!-- ### -->
+<script>
+module.exports = {
+}
+</script>
