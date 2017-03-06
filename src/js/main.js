2c01cb2a0c23fb355652c47e4af0659858178b66
Eric Zanchi <emzanchi@gmail.com>
2017-03-06 11:49:39 -0600

    Removed unnecessary imports

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,9 @@
 import Vue from 'vue';
 import router from './router';
 import store from 'Store';
-import {roundUpdate, updateRoom} from 'WebSocket/streams';
 
 import fontLoad from './fontLoad';
 fontLoad();
-
-// Component loading and put in objects so they can be called by string name later
-import Create from 'Dialogs/Create.vue';
-import Join from 'Dialogs/Join.vue';
-import UpdateName from 'Dialogs/UpdateName.vue';
 
 /* Main app instance */
 var app = new Vue({
